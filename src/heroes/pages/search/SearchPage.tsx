533d1e7fda528a3e03909acb503ccfa013a8445b
Developer <test@example.com>
2025-09-14 20:16:04 -0500

    ✨ feat: Añadir componente HeroStats con tarjetas de estadísticas reutilizables
    - Crea `HeroStats` para centralizar la visualización de estadísticas de personajes
    - Introduce `HeroStatCard` como componente reutilizable para cada tarjeta
    - Refactoriza `HomePage` y `SearchPage` reemplazando bloques de UI con `HeroStats`
    - Mejora organización al separar lógica de las tarjetas en componentes individuales

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,12 +1,16 @@
 import {CustomJumbotron} from "@/components/custom/CustomJumbotron.tsx";
+import {HeroStats} from "@/heroes/components/HeroStats.tsx";
 
 const SearchPage = () => {
   return (
     <>
+      {/*Header*/}
       <CustomJumbotron
         title={'Busqueda de Superheroes'}
         description={'Descubre, explora y administra tus superheroes y villanos favoritos'}
       />
+      {/* Stats Dashboard */}
+      <HeroStats/>
     </>
   )
 }
