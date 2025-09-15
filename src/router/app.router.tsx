ff8470efe9d3b71cf12fa68e793ed7ed632f2f9d
Developer <test@example.com>
2025-09-14 19:20:47 -0500

    ✨ feat: Introducir layouts para rutas principales
    - Añade `HeroesLayout` y `AdminLayout` para encapsular vistas relacionadas
    - Actualiza rutas en `app.router.tsx` con estructura anidada y soporte para layouts
    - Implementa `Navigate` para redireccionar rutas no existentes al inicio
    - Mejora la organización del código para vistas y layouts principales

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,29 +1,43 @@
-import {createBrowserRouter} from "react-router";
+import {createBrowserRouter, Navigate} from "react-router";
 import {HomePage} from "@/heroes/pages/home/HomePage.tsx";
 import {HeroPage} from "@/heroes/pages/hero/HeroPage.tsx";
 import {SearchPage} from "@/heroes/pages/search/SearchPage.tsx";
 import {AdminPage} from "@/admin/pages/AdminPage.tsx";
+import {HeroesLayout} from "@/heroes/layouts/HeroesLayout.tsx";
+import {AdminLayout} from "@/admin/layouts/AdminLayout.tsx";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage/>
-  },
-  {
-    path: "/heroes/1",
-    element: <HeroPage/>
-  },
-  {
-    path: "/search",
-    element: <SearchPage/>
+    element: <HeroesLayout/>,
+    children: [
+      {
+        index: true,
+        element: <HomePage/>
+      },
+      {
+        path: "heroes/1",
+        element: <HeroPage/>
+      },
+      {
+        path: "search",
+        element: <SearchPage/>
+      },
+    ]
   },
   {
     path: "/admin",
-    element: <AdminPage/>
+    element: <AdminLayout/>,
+    children: [
+      {
+        index: true,
+        element: <AdminPage/>
+      },
+    ]
   },
   {
     path: "*",
-    element: <HomePage/>
+    element: <Navigate to={'/'}/>
   },
 
 ])
